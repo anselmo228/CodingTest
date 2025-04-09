2c6c1b72001e6024d4dd5d2334ee99bac4bdfb03
정희찬 <anselmo228@naver.com>
2025-04-09 17:31:51 +0900

    Daily Coding

--- 13335.py
+++ 13335.py
@@ -3,25 +3,26 @@ from collections import deque
 n, w, l = map(int, input().split())
 trucks = list(map(int, input().split()))
 
-queue = deque()
-
-for i in range(len(trucks)):
-    queue.append(trucks[i])
-
-weight = 0
-length = 0
+bridge = deque([0] * w)  # 다리의 초기 상태: w 길이만큼 0
 time = 0
+current_weight = 0
+truck_index = 0
 
-while queue:
-    truck = queue.popleft()
-    weight += truck
-    length += 1
+while truck_index < n:
+    time += 1
+    # 다리의 앞쪽에서 트럭 빠짐
+    out_truck = bridge.popleft()
+    current_weight -= out_truck
 
-    if weight > w and length - 1 <= l:
-        weight = truck
-        length = 1
-        time += 1
-    elif weight > w and length - 1 > l:
-        weight = truck 
+    # 다음 트럭이 올라갈 수 있으면 올림
+    if current_weight + trucks[truck_index] <= l:
+        bridge.append(trucks[truck_index])
+        current_weight += trucks[truck_index]
+        truck_index += 1
+    else:
+        bridge.append(0)  # 트럭 안 올라가고 시간만 흐름
+
+# 마지막 트럭이 다리 끝까지 가는 시간 더하기
+time += w
 
 print(time)
